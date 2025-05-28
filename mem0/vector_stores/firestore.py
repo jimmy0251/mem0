0fb4225b3e8e430bd09097d1f4e0fb7918e6951f
Jimmy S <jimmy0251@gmail.com>
2025-05-28 22:06:58 +0530

    Minor changes

--- mem0/vector_stores/firestore.py
+++ mem0/vector_stores/firestore.py
@@ -8,7 +8,8 @@ from pydantic import BaseModel
 try:
     import firebase_admin
     from firebase_admin import credentials, firestore
-    from google.cloud.firestore_v1.base_vector_query import DistanceMeasure, Vector
+    from google.cloud.firestore_v1.base_vector_query import DistanceMeasure
+    from google.cloud.firestore_v1.vector import Vector
     from google.cloud.firestore import Client, CollectionReference # Corrected imports
 except ImportError:
     raise ImportError(
